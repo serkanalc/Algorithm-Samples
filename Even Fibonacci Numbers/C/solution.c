7893bf2c033dd3f2da5b86c2fabff25f5a58eedf
Serkan Alc <70329389+serkanalc@users.noreply.github.com>
2022-03-06 05:12:13 +0300

    Create solution.c

---
+++ Even Fibonacci Numbers/C/solution.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  unsigned int a1 = 1, a2 = 1, a3 = 2, sum = 0;
+
+  while (a3 < 2000000) {
+    a3 = a1 + a2;
+    sum += a3 * !(a3%2);
+    a1 = a2;
+    a2 = a3;
+  }
+
+  printf("%u\n", sum);
+
+  return 0;
+}
